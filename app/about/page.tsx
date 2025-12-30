47851babe564fc29efdc8dfa3722a037d892c239
Per-Olof Svensson <per-olof.svensson@knowit.se>
2025-12-30 20:14:11 +0100

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <main style={{ padding: '2rem' }}>
       <h1>About Page</h1>
-      <p>Denna sida används som mål för hover-prefetch.</p>
+      <p>Denna sida används som mål för hover-prefetch..</p>
     </main>
   );
 }
